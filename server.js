f65956d60cd0b05b9b128637cf5f779e6fc4bb12
Jonas Hörström <jonas.horstrom@gmail.com>
2018-07-05 22:52:27 +0200

    Update lint and fix issues

--- server.js
+++ server.js
@@ -13,15 +13,16 @@ app.use(serve(`${__dirname}/dist`));
 
 // rest endpoint
 router
-    .get('/api/', (ctx) => {
-      ctx.body = 'I am listening!';
-      console.log('GET');
-    })
-    .post('/api/happiness', koaBody, (ctx) => {
-      console.log(ctx.request.body);
-      ctx.body = JSON.stringify(ctx.request.body, null, 2);
-      ctx.status = 201;
-    });
+  .get('/api/', (ctx) => {
+    ctx.body = 'I am listening!';
+    console.log('GET');
+  })
+  .post('/api/happiness', koaBody, (ctx) => {
+    console.log(ctx.request.body);
+    ctx.body = JSON.stringify(ctx.request.body, null, 2);
+    console.log(ctx.body);
+    ctx.status = 201;
+  });
 app.use(router.routes());
 
 app.listen(8080);
